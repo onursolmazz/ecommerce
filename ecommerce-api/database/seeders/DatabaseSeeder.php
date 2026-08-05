58f8bed92ed2650c2e0d9030d73e57873f1a1bc9
onursolmazz <onursolmaz2323@gmail.com>
2026-08-05 13:32:09 +0300

    bug fix

--- ecommerce-api/database/seeders/DatabaseSeeder.php
+++ ecommerce-api/database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             // RoleSeeder::class,
             // CategorySeeder::class,
             // UserSeeder::class,
-            ProductSeeder::class,
+            // ProductSeeder::class,
             // ProductImageSeeder::class,
 
         ]);
