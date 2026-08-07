7edcc91a83b3a225bdf54d732ecae50c4435ffbb
onursolmazz <onursolmaz2323@gmail.com>
2026-08-07 16:32:06 +0300

    added url

--- ecommerce-web/src/utils/globals.js
+++ ecommerce-web/src/utils/globals.js
@@ -1,5 +1,5 @@
 const globals = {
-  url: "https://ecommerce-du83.vercel.app",
+  url: "https://onursolmaz.online",
   apiUrl: "https://ecommerce-w7ko.onrender.com/api",
   imageUrl: "https://ecommerce-w7ko.onrender.com/storage",
   defaultProductImage: "/images/no-image.png",
